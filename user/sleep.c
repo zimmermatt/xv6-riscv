ee4e963d7ccf7fb238c3b12a94c1581d5090b2f5
Matt Zimmer <zimmermatt@gmail.com>
2025-01-12 20:54:41 -0500

    Implement a `sleep` utility
    This is week 1 Assigment #3
    https://github.com/thatch/operating-systems-with-xv6/blob/main/week01.md

---
+++ user/sleep.c
@@ -0,0 +1,34 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc < 2 || argc > 3){
+    fprintf(2, "Usage: sleep [--snore] seconds_to_sleep\n");
+    exit(1);
+  }
+
+  int sleep_arg_idx = 1;
+  int snore = 0;
+
+  if (argc == 3) {
+    if (strcmp("--snore", argv[1]) == 0) {
+      snore = 1;
+      sleep_arg_idx = 2;
+    } else {
+      fprintf(2, "Usage: sleep [--snore] seconds_to_sleep\n");
+      exit(1);
+    }
+  }
+
+  int sleep_seconds = atoi(argv[sleep_arg_idx]);
+  while(sleep_seconds-- > 0) {
+    if (snore) printf("z");
+    sleep(1);
+  }
+
+  if (snore) printf("\n");
+
+  exit(0);
+}
